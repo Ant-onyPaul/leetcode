d0d3fab7563021e3d8fa9763c1c04ed368e3cdae
Antony Paul <antonypaulachandy@gmail.com>
2026-09-03 20:02:39 +0530

    Time: 60 ms (62.19%) | Memory: 35.5 MB (18.90%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def uniformArray(self, nums1: list[int]) -> bool:
+
+        nums1.sort()
+
+        if nums1[0] % 2 == 1:
+            return True
+
+        for x in nums1:
+            if x % 2 == 1:
+                return False
+
+        return True
